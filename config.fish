ebc89eeff79d9ac47892c0ea3e0f4e75216bd598
蒋慧赟 <42379289+cnjhb@users.noreply.github.com>
2024-03-16 01:52:13 +0000

    Update config.fish

--- config.fish
+++ config.fish
@@ -5,3 +5,8 @@ end
 function fish_right_prompt -d "Write out the right prompt"
     set_color $fish_color_status; date "+%H:%M:%S"
 end
+set -x EDITOR nvim
+
+set -g fish_key_bindings fish_vi_key_bindings
+
+bind -M insert \cf accept-autosuggestion
