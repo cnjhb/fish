be57548173505f720224bec86497bad8f42c1abc
蒋慧赟 <42379289+cnjhb@users.noreply.github.com>
2025-02-12 16:01:23 +0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,12 +1,10 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    function fish_right_prompt -d "Write out the right prompt"
+	set_color $fish_color_status; date "+%H:%M:%S"
+    end
+    set -x EDITOR nvim
+
+    set -g fish_key_bindings fish_vi_key_bindings
+
+    bind -M insert \cf accept-autosuggestion
 end
-
-function fish_right_prompt -d "Write out the right prompt"
-    set_color $fish_color_status; date "+%H:%M:%S"
-end
-set -x EDITOR nvim
-
-set -g fish_key_bindings fish_vi_key_bindings
-
-bind -M insert \cf accept-autosuggestion
