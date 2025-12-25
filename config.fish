9764dbb90b28cafe37c7ecb4cff68088fe3b768b
蒋慧赟 <42379289+cnjhb@users.noreply.github.com>
2025-12-25 15:39:45 +0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,10 +1,16 @@
 if status is-interactive
-    function fish_right_prompt -d "Write out the right prompt"
-	set_color $fish_color_status; date "+%H:%M:%S"
-    end
     set -x EDITOR nvim
 
     set -g fish_key_bindings fish_vi_key_bindings
 
     bind -M insert \cf accept-autosuggestion
+
+	if type -q busybox
+		for cmd in (busybox --list)
+			if not type -q $cmd
+				abbr $cmd "busybox $cmd"
+			end
+		end
+	end
+
 end
